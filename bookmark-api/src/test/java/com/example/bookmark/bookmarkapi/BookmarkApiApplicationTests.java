29d503135a5ed0fc4a15c2d18cbad7e7660586dc
sachinsubudhi <sachin.subudhi@gmail.com>
2023-09-07 13:49:16 +0530

    Initial commit for bookmark-api

---
+++ bookmark-api/src/test/java/com/example/bookmark/bookmarkapi/BookmarkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmark.bookmarkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
